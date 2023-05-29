4dbbf43646d4fc20412251f93e2c5951192b9000
aysuxalilli <129958383+aysuxalilli@users.noreply.github.com>
2023-05-29 19:39:41 +0400

    Revert "Aysu"

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 import {Root} from './navigation/Root';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-const App = () => (
-  <SafeAreaProvider>
-    <Root />
-  </SafeAreaProvider>
-);
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <Root />
+    </SafeAreaProvider>
+  );
+};
 export default App;
